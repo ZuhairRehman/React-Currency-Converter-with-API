b5f45601f7c36cfbc3cc83b2e46344296992dd96
Zuhair Rehman <zuhair.rehman85@gmail.com>
2024-07-04 06:37:16 +0100

    Have to add more control on UI

--- src/Hooks/useCurrencyInfo.js
+++ src/Hooks/useCurrencyInfo.js
@@ -1,18 +1,15 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency) {
+export default function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
 
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
     )
       .then((res) => res.json())
-      .then((data) => setData(res[currency]));
-    console.log(data);
+      .then((res) => setData(res[currency]));
   }, [currency]);
   console.log(data);
   return data;
 }
-
-export default useCurrencyInfo;
